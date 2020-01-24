4d0c6d31f96b2ce89230ed95c16bba65b23da0e4
Rodrigo Montes <rjmontes@gmail.com>
2020-01-24 16:54:10 -0300

    Video 47 - Sound Effects!
    - Added sound to rocket (audio file from www.freesound.org)

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -6,28 +6,39 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioSource;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
-        
+        ProcessInput(); 
     }
 
     private void ProcessInput()
     {
+        // thrust
         if (Input.GetKey(KeyCode.Space))        // separated IF cause can thrust and rotate at the same time
         {
             rigidBody.AddRelativeForce(Vector3.up);
+
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        } else
+        {
+            audioSource.Stop();
         }
 
+        // direction
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.forward);
