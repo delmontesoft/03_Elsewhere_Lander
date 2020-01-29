507b212670d73e53bd56c3a92cfad40554daa846
Rodrigo Montes <rjmontes@gmail.com>
2020-01-29 20:56:42 -0300

    Video 61 - Moving Obstacles
    - Added a new script Oscillator.cs to move objects (obstacles):
    * It moves in the 3 axis independently by the amount set in "Movement Vector"
    * It takes X seconds to move from begin to end as set on "Movement Time"
    - Added moving obstacle to lelvel 1 (v0.3)

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]     // Won let add this component more than once to a object
+public class Oscillator : MonoBehaviour
+{
+    // Oscillator settings
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);   // amount of movement in units per axis
+    [SerializeField] float movementTime = 2f;           // period in seconds (period from sin function)
+    float movementFactor;       // add [Range(0,1)] to variable to force between 0 and 1 (percentage)
+
+    Vector3 startingPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // TODO protect agains movementTime = 0 => you can't divide by 0!!
+        float cycles = Time.time / movementTime;       // Time.time is framerate independant, so no need to do Time.deltaTime
+        const float tau = Mathf.PI * 2;     //tau is about 6.28
+        float rawSinWave = Mathf.Sin(cycles * tau);     // goes from -1 to +1
+
+        movementFactor = rawSinWave / 2f + 0.5f;        // to force it to go between 0 and 1 (from -1 to +1)
+        Vector3 movementOffset = movementVector * movementFactor;
+        transform.position = startingPosition + movementOffset;
+    }
+}
